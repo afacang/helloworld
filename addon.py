582d67a9ebe20d2fb0b8e7d4034e2ce9141b236c
afacang <gokhan.afacan@gmail.com>
2016-06-04 00:43:45 +0200

    vpn1 commit

--- addon.py
+++ addon.py
@@ -1,11 +1,25 @@
 import xbmcaddon
 import xbmcgui
+import socket
+import fcntl
+import struct
  
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
+line1 = "Hello "
+line2 = "Your VPN Address is"
 line3 = "Using Python"
  
-xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+#get_ip_address('tun0') 
+
+xbmcgui.Dialog().ok(addonname, line1, line2, get_ip_address('tun0'))
